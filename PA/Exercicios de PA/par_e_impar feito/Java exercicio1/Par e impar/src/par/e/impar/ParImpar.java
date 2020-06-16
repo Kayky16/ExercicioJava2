8da788d596fa3e21370e1e30b1ae7ba44d64fe3d
Kayky16 <kayky.morais01@etec.sp.gov.br>
2020-06-15 21:19:20 -0300

    Primeiro exercicio

---
+++ PA/Exercicios de PA/par_e_impar feito/Java exercicio1/Par e impar/src/par/e/impar/ParImpar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*/
+
+package par.e.impar;
+
+import java.util.Scanner;
+
+public class ParImpar {
+    
+    public static void main(String[] args) {
+       
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n = entrada.nextInt();
+        
+        System.out.println(n%2==0? "Par": "Ímpar");
+    }
+    
+}
